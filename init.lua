9e615e02724969a4e3bf6bc919964950e90c15ad
Adrián Asensio <aasensiomtd@gmail.com>
2026-05-15 20:48:33 +0200

    Removed partialy nvchad

--- init.lua
+++ init.lua
@@ -14,28 +14,18 @@ vim.opt.rtp:prepend(lazypath)
 local lazy_config = require "configs.lazy"
 
 -- load plugins
--- require("lazy").setup({
---   {
---     "NvChad/NvChad",
---     lazy = false,
---     branch = "v2.5",
---     import = "nvchad.plugins",
---   },
---
---   { import = "plugins" },
--- }, lazy_config)
---
-
 require("lazy").setup({
-   {
-     dir = "~/Documents/programming/NvChad",
-     lazy = false,
-     branch = "v2.5",
-     import = "nvchad.plugins",
-   },
+  {
+    "eyohadrian/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
 
   { import = "plugins" },
 }, lazy_config)
+--
+--
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
