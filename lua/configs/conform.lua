d02f2ae9c2d1c9452c78edf7e67fe9d8ee293532
Adrián Asensio <aasensiomtd@gmail.com>
2026-06-01 11:44:43 +0200

    Conform for c and cpp

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,19 +1,15 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    json  = { "prettier" },
+    json = { "prettier" },
     jsonc = { "prettier" },
     yaml = { "prettier" },
     python = { "ruff_fix", "black" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    cpp = { "clang_format" },
+    c = { "clang_format" },
+    css = { "prettier" },
+    html = { "prettier" },
   },
-
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
 }
 
 return options
