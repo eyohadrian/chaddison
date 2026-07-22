57111c6eade0a653b369a670ab8a555b4286c041
Adrián Asensio <aasensiomtd@gmail.com>
2026-07-22 09:51:13 +0200

    Leet code plugin

---
+++ lua/plugins/leet.lua
@@ -0,0 +1,14 @@
+return {
+  'kawre/leetcode.nvim',
+  -- enabled = false,
+  cmd = 'Leet',
+  dependencies = {
+    'ibhagwan/fzf-lua',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+  },
+  opts = {
+    -- lang = 'cpp',
+    lang = 'python3',
+  },
+}
