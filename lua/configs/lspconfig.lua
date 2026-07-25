2e818d5dc2a1e3473fdb58c484029b388324e338
Adrian <adrian.asensio.bdn@gmail.com>
2026-07-25 17:46:43 +0200

    Auto find .venv in buffer project for python

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,24 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" , "pyright", "clangd", "lua_ls"}
+local servers = { "html", "cssls", "clangd", "lua_ls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- Auto find .venv in buffer project 
+vim.lsp.config("pyright", {
+  before_init = function(_, config)
+    local root_dir = config.root_dir
+    if not root_dir then
+      return
+    end
+
+    local python = vim.fs.joinpath(root_dir, ".venv", "bin", "python")
+
+    if vim.fn.executable(python) == 1 then
+      config.settings = config.settings or {}
+      config.settings.python = config.settings.python or {}
+      config.settings.python.pythonPath = python
+    end
+  end,
+})
+
+vim.lsp.enable("pyright")
